fc5c8ee466c62d8dc0b9104b00644b592d3e4edc
Huy <87337718+huytripactions@users.noreply.github.com>
2024-12-12 15:02:16 -0800

    Initial setup (#1)

--- infra/redis/src/main/java/com/tripactions/infra/redis/queue/MessagePublisher.java
+++
@@ -1,5 +0,0 @@
-package com.tripactions.infra.redis.queue;
-
-public interface MessagePublisher {
-	void publish(final String message);
-}
--- infra/redis/src/main/java/com/tripactions/infra/redis/repo/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tripactions.infra.redis.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.tripactions.infra.redis.model.Person;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, String> {}
